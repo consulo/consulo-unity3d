017244322f83f5f6d107f67f92cc4e2ce25563a7
VISTALL <vistall.valeriy@gmail.com>
2014-12-23 21:46:13 +0300

    stop process if we failed to connect

--- src/org/mustbe/consulo/unity3d/run/UnityRemoteDebugRunner.java
+++ src/org/mustbe/consulo/unity3d/run/UnityRemoteDebugRunner.java
@@ -20,16 +20,20 @@ import java.util.List;
 
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import org.mustbe.consulo.dotnet.debugger.DotNetVirtualMachineListener;
 import org.mustbe.consulo.dotnet.execution.DebugConnectionInfo;
 import org.mustbe.consulo.unity3d.run.debugger.UnityDebugProcess;
 import org.mustbe.consulo.unity3d.run.debugger.UnityPlayer;
 import org.mustbe.consulo.unity3d.run.debugger.UnityProcessDialog;
 import com.intellij.execution.ExecutionException;
+import com.intellij.execution.actions.StopProcessAction;
 import com.intellij.execution.configurations.RunProfile;
 import com.intellij.execution.configurations.RunProfileState;
 import com.intellij.execution.executors.DefaultDebugExecutor;
+import com.intellij.execution.process.ProcessHandler;
 import com.intellij.execution.runners.DefaultProgramRunner;
 import com.intellij.execution.runners.ExecutionEnvironment;
+import com.intellij.execution.ui.ConsoleViewContentType;
 import com.intellij.execution.ui.RunContentDescriptor;
 import com.intellij.openapi.fileEditor.FileDocumentManager;
 import com.intellij.util.containers.ContainerUtil;
@@ -37,6 +41,8 @@ import com.intellij.xdebugger.XDebugProcess;
 import com.intellij.xdebugger.XDebugProcessStarter;
 import com.intellij.xdebugger.XDebugSession;
 import com.intellij.xdebugger.XDebuggerManager;
+import lombok.val;
+import mono.debugger.VirtualMachine;
 
 /**
  * @author VISTALL
@@ -59,8 +65,8 @@ public class UnityRemoteDebugRunner extends DefaultProgramRunner
 
 	@Nullable
 	@Override
-	protected RunContentDescriptor doExecute(@NotNull RunProfileState state, @NotNull final ExecutionEnvironment environment) throws
-			ExecutionException
+	protected RunContentDescriptor doExecute(@NotNull RunProfileState state,
+			@NotNull final ExecutionEnvironment environment) throws ExecutionException
 	{
 		UnityProcessDialog dialog = new UnityProcessDialog(environment.getProject());
 
@@ -75,17 +81,40 @@ public class UnityRemoteDebugRunner extends DefaultProgramRunner
 		FileDocumentManager.getInstance().saveAllDocuments();
 		final XDebugSession debugSession = XDebuggerManager.getInstance(environment.getProject()).startSession(environment,
 				new XDebugProcessStarter()
+		{
+			@NotNull
+			@Override
+			public XDebugProcess start(@NotNull final XDebugSession session) throws ExecutionException
+			{
+				DebugConnectionInfo debugConnectionInfo = new DebugConnectionInfo(firstItem.getIp(), firstItem.getDebuggerPort(), true);
+				val process = new UnityDebugProcess(session, debugConnectionInfo, environment.getRunProfile());
+				process.getDebugThread().addListener(new DotNetVirtualMachineListener()
 				{
-					@NotNull
 					@Override
-					public XDebugProcess start(@NotNull XDebugSession session) throws ExecutionException
+					public void connectionSuccess(@NotNull VirtualMachine machine)
 					{
-						DebugConnectionInfo debugConnectionInfo = new DebugConnectionInfo(firstItem.getIp(), firstItem.getDebuggerPort(), true);
-						UnityDebugProcess process = new UnityDebugProcess(session, debugConnectionInfo, environment.getRunProfile());
-						process.start();
-						return process;
+						session.getConsoleView().print(String.format("Success attach to Unity process at %s:%d", firstItem.getIp(),
+								firstItem.getDebuggerPort()), ConsoleViewContentType.SYSTEM_OUTPUT);
+					}
+
+					@Override
+					public void connectionStopped()
+					{
+					}
+
+					@Override
+					public void connectionFailed()
+					{
+						ProcessHandler processHandler = process.getProcessHandler();
+						session.getConsoleView().print(String.format("Failed attach to Unity process at %s:%d", firstItem.getIp(),
+								firstItem.getDebuggerPort()), ConsoleViewContentType.ERROR_OUTPUT);
+						StopProcessAction.stopProcess(processHandler);
 					}
 				});
+				process.start();
+				return process;
+			}
+		});
 
 		return debugSession.getRunContentDescriptor();
 	}
