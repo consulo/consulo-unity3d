8e15952272f31ab163272f7bb0a2224323e2cf07
VISTALL <vistall.valeriy@gmail.com>
2015-08-22 13:37:20 +0300

    fixed CEP-1194

--- src/org/mustbe/consulo/unity3d/action/SyncUnity3dProjectAction.java
+++ src/org/mustbe/consulo/unity3d/action/SyncUnity3dProjectAction.java
@@ -11,6 +11,8 @@ import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.progress.ProgressIndicator;
 import com.intellij.openapi.progress.Task;
+import com.intellij.openapi.project.DumbModePermission;
+import com.intellij.openapi.project.DumbService;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
 
@@ -46,7 +48,14 @@ public class SyncUnity3dProjectAction extends AnAction
 			public void run(@NotNull ProgressIndicator indicator)
 			{
 				indicator.setIndeterminate(true);
-				Unity3dProjectUtil.importOrUpdate(project, rootModuleExtension.getSdk(), null);
+				DumbService.allowStartingDumbModeInside(DumbModePermission.MAY_START_BACKGROUND, new Runnable()
+				{
+					@Override
+					public void run()
+					{
+						Unity3dProjectUtil.importOrUpdate(project, rootModuleExtension.getSdk(), null);
+					}
+				});
 			}
 		}.queue();
 	}
