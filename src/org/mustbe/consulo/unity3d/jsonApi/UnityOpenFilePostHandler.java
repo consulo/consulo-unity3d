5b3d897aebf81c3be626eda2add21a433200113a
VISTALL <vistall.valeriy@gmail.com>
2016-01-04 16:04:09 +0300

    open project if it closed

--- src/org/mustbe/consulo/unity3d/jsonApi/UnityOpenFilePostHandler.java
+++ src/org/mustbe/consulo/unity3d/jsonApi/UnityOpenFilePostHandler.java
@@ -16,19 +16,26 @@
 
 package org.mustbe.consulo.unity3d.jsonApi;
 
+import java.awt.Frame;
 import java.util.Set;
 
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.mustbe.buildInWebServer.api.JsonPostRequestHandler;
 import org.mustbe.buildInWebServer.api.RequestFocusHttpRequestHandler;
 import com.intellij.openapi.fileEditor.FileEditorManager;
 import com.intellij.openapi.fileEditor.OpenFileDescriptor;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.project.ProjectManager;
+import com.intellij.openapi.ui.Messages;
 import com.intellij.openapi.vfs.LocalFileSystem;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.openapi.wm.IdeFrame;
 import com.intellij.openapi.wm.WindowManager;
+import com.intellij.util.BitUtil;
 import com.intellij.util.containers.ContainerUtil;
 import com.intellij.util.ui.UIUtil;
 
@@ -74,6 +81,27 @@ public class UnityOpenFilePostHandler extends JsonPostRequestHandler<UnityOpenFi
 						}
 					}
 
+					if(openedProject == null)
+					{
+						VirtualFile projectDir = projectVirtualFile.findChild(Project.DIRECTORY_STORE_FOLDER);
+						if(projectDir == null)
+						{
+							JFrame visibleFrame = WindowManager.getInstance().findVisibleFrame();
+							activateFrame(visibleFrame);
+							Messages.showErrorDialog("Project with path: " + body.projectPath + " is not imported", "Consulo");
+							return;
+						}
+
+						try
+						{
+							openedProject = ProjectManager.getInstance().loadAndOpenProject(projectVirtualFile.getPath());
+						}
+						catch(Exception e)
+						{
+							Messages.showErrorDialog("Fail to open project by path: " + projectVirtualFile.getPath(), "Consulo");
+						}
+					}
+
 					if(openedProject != null)
 					{
 						IdeFrame ideFrame = WindowManager.getInstance().getIdeFrame(openedProject);
@@ -90,5 +118,34 @@ public class UnityOpenFilePostHandler extends JsonPostRequestHandler<UnityOpenFi
 			}
 		});
 		return JsonResponse.asSuccess(null);
+	}
+
+	public static boolean activateFrame(@Nullable final JFrame frame)
+	{
+		if(frame != null)
+		{
+			Runnable runnable = new Runnable()
+			{
+				@Override
+				public void run()
+				{
+					int extendedState = frame.getExtendedState();
+					if(BitUtil.isSet(extendedState, Frame.ICONIFIED))
+					{
+						extendedState = BitUtil.set(extendedState, Frame.ICONIFIED, false);
+						frame.setExtendedState(extendedState);
+					}
+
+					// fixme [vistall] dirty hack - show frame on top
+					frame.setAlwaysOnTop(true);
+					frame.setAlwaysOnTop(false);
+					frame.requestFocus();
+				}
+			};
+			//noinspection SSBasedInspection
+			SwingUtilities.invokeLater(runnable);
+			return true;
+		}
+		return false;
 	}
 }
