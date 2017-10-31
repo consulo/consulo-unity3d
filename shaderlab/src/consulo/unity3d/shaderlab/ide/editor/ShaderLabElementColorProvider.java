ab7653afccc903e91b6dc29ab5a46457dd61dcfb
VISTALL <vistall.valeriy@gmail.com>
2017-10-31 10:11:56 +0300

    implement value set from color provider

--- shaderlab/src/consulo/unity3d/shaderlab/ide/editor/ShaderLabElementColorProvider.java
+++ shaderlab/src/consulo/unity3d/shaderlab/ide/editor/ShaderLabElementColorProvider.java
@@ -22,10 +22,16 @@ import java.util.List;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import com.intellij.openapi.editor.ElementColorProvider;
+import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFileFactory;
+import consulo.annotations.RequiredReadAction;
+import consulo.annotations.RequiredWriteAction;
+import consulo.unity3d.shaderlab.lang.ShaderLabFileType;
 import consulo.unity3d.shaderlab.lang.ShaderLabPropertyType;
 import consulo.unity3d.shaderlab.lang.parser.roles.ShaderLabColorRole;
 import consulo.unity3d.shaderlab.lang.parser.roles.ShaderLabRole;
+import consulo.unity3d.shaderlab.lang.psi.ShaderLabFile;
 import consulo.unity3d.shaderlab.lang.psi.ShaderLabTokens;
 import consulo.unity3d.shaderlab.lang.psi.ShaderProperty;
 import consulo.unity3d.shaderlab.lang.psi.ShaderPropertyType;
@@ -38,6 +44,7 @@ import consulo.unity3d.shaderlab.lang.psi.ShaderSimpleValue;
  */
 public class ShaderLabElementColorProvider implements ElementColorProvider
 {
+	@RequiredReadAction
 	@Nullable
 	@Override
 	public Color getColorFrom(@NotNull PsiElement element)
@@ -106,9 +113,37 @@ public class ShaderLabElementColorProvider implements ElementColorProvider
 		return false;
 	}
 
+	@RequiredWriteAction
 	@Override
 	public void setColorTo(@NotNull PsiElement element, @NotNull Color color)
 	{
+		float[] colorComponents = color.getRGBComponents(null);
 
+		StringBuilder builder = new StringBuilder();
+		builder.append("(");
+		for(int i = 0; i < (colorComponents[3] == 1 ? colorComponents.length - 1 : colorComponents.length); i++)
+		{
+			if(i != 0)
+			{
+				builder.append(", ");
+			}
+
+			builder.append(colorComponents[i]);
+		}
+		builder.append(")");
+
+		ShaderPropertyValue value = createValue(element.getProject(), builder.toString());
+
+		element.replace(value);
+	}
+
+	@NotNull
+	@RequiredReadAction
+	private static ShaderPropertyValue createValue(Project project, String text)
+	{
+		String full = "Shader \"dummy\" { Properties { _dummy(\"dummy\", Color)  = " + text + " } }";
+		ShaderLabFile psiFile = (ShaderLabFile) PsiFileFactory.getInstance(project).createFileFromText(full, ShaderLabFileType.INSTANCE, full);
+
+		return psiFile.getProperties().get(0).getValue();
 	}
 }
