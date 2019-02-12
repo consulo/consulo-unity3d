06e363d0fc0048912342b14236f870bfdc709756
Valery Semenchuk <vistall.valeriy@gmail.com>
2019-02-12 08:10:56 +0300

    fix compilation

--- plugin/src/main/java/consulo/unity3d/usages/Unity3dAssetUsageFilteringRuleProvider.java
+++ plugin/src/main/java/consulo/unity3d/usages/Unity3dAssetUsageFilteringRuleProvider.java
@@ -14,7 +14,6 @@ import com.intellij.usages.rules.PsiElementUsage;
 import com.intellij.usages.rules.UsageFilteringRule;
 import com.intellij.usages.rules.UsageFilteringRuleProvider;
 import consulo.annotations.RequiredDispatchThread;
-import consulo.awt.TargetAWT;
 import consulo.unity3d.Unity3dIcons;
 import consulo.unity3d.module.Unity3dModuleExtensionUtil;
 import consulo.unity3d.scene.Unity3dYMLAssetFileType;
@@ -29,7 +28,7 @@ public class Unity3dAssetUsageFilteringRuleProvider implements UsageFilteringRul
 	{
 		private ShowAssetUsageAction(UsageView view)
 		{
-			super(view, "Show assets usage", TargetAWT.to(Unity3dIcons.Unity3d));
+			super(view, "Show assets usage", Unity3dIcons.Unity3d);
 		}
 
 		@RequiredDispatchThread
