0fffb289e4143a83dcbba563ac61c2e789ad7992
Valery Semenchuk <vistall.valeriy@gmail.com>
2019-05-14 11:54:09 +0300

    add checkCanceled

--- plugin/src/main/java/consulo/unity3d/unityscript/projectView/UnityScriptFileProjectViewProvider.java
+++ plugin/src/main/java/consulo/unity3d/unityscript/projectView/UnityScriptFileProjectViewProvider.java
@@ -16,25 +16,28 @@
 
 package consulo.unity3d.unityscript.projectView;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-
-import javax.inject.Inject;
-
 import com.intellij.ide.projectView.TreeStructureProvider;
 import com.intellij.ide.projectView.ViewSettings;
 import com.intellij.ide.util.treeView.AbstractTreeNode;
+import com.intellij.ide.util.treeView.AbstractTreeUi;
 import com.intellij.lang.javascript.psi.JSFile;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.module.ModuleUtilCore;
+import com.intellij.openapi.progress.ProgressManager;
 import com.intellij.openapi.project.DumbAware;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
+import consulo.annotations.RequiredReadAction;
 import consulo.ui.RequiredUIAccess;
 import consulo.unity3d.module.Unity3dModuleExtensionUtil;
 import consulo.unity3d.module.Unity3dRootModuleExtension;
+
+import javax.annotation.Nonnull;
+import javax.inject.Inject;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 /**
  * @author VISTALL
@@ -54,19 +57,24 @@ public class UnityScriptFileProjectViewProvider implements TreeStructureProvider
 	@RequiredUIAccess
 	public Collection<AbstractTreeNode> modify(AbstractTreeNode parent, Collection<AbstractTreeNode> children, ViewSettings settings)
 	{
-		if(!myProject.isInitialized())
-		{
-			return children;
-		}
+		return AbstractTreeUi.calculateYieldingToWriteAction(() -> doModify(children, settings));
+	}
+
+	@RequiredReadAction
+	@Nonnull
+	private Collection<AbstractTreeNode> doModify(Collection<AbstractTreeNode> children, ViewSettings settings)
+	{
 		Unity3dRootModuleExtension rootModuleExtension = Unity3dModuleExtensionUtil.getRootModuleExtension(myProject);
 		if(rootModuleExtension == null)
 		{
 			return children;
 		}
 
-		List<AbstractTreeNode> nodes = new ArrayList<AbstractTreeNode>(children.size());
+		List<AbstractTreeNode> nodes = new ArrayList<>(children.size());
 		for(AbstractTreeNode child : children)
 		{
+			ProgressManager.checkCanceled();
+
 			Object value = child.getValue();
 			if(value instanceof JSFile)
 			{
