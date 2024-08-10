99cfd2752ca9ab9ccef970791ead59881f7d0a33
Valery Semenchuk <vistall.valeriy@gmail.com>
2024-08-10 16:51:57 +0400

    migrate to activity

--- plugin/src/main/java/consulo/unity3d/projectImport/change/Unity3dProjectChangeListener.java
+++ plugin/src/main/java/consulo/unity3d/projectImport/change/Unity3dProjectChangeListener.java
@@ -21,6 +21,7 @@ import consulo.annotation.component.ComponentScope;
 import consulo.annotation.component.ServiceAPI;
 import consulo.annotation.component.ServiceImpl;
 import consulo.application.Application;
+import consulo.application.ReadAction;
 import consulo.application.util.concurrent.AppExecutorUtil;
 import consulo.disposer.Disposable;
 import consulo.language.util.ModuleUtilCore;
@@ -29,11 +30,9 @@ import consulo.module.content.layer.event.ModuleRootEvent;
 import consulo.module.content.layer.event.ModuleRootListener;
 import consulo.project.DumbService;
 import consulo.project.Project;
-import consulo.project.startup.StartupManager;
 import consulo.project.ui.notification.Notification;
 import consulo.project.ui.notification.NotificationAction;
 import consulo.project.ui.notification.NotificationType;
-import consulo.ui.UIAccess;
 import consulo.ui.annotation.RequiredUIAccess;
 import consulo.ui.ex.action.AnActionEvent;
 import consulo.ui.ex.awt.UIUtil;
@@ -70,190 +69,155 @@ import java.util.concurrent.atomic.AtomicBoolean;
  * @since 1/11/18
  */
 @Singleton
-@ServiceAPI(value = ComponentScope.PROJECT, lazy = false)
+@ServiceAPI(value = ComponentScope.PROJECT)
 @ServiceImpl
-public class Unity3dProjectChangeListener implements Disposable
-{
-	public static class DataBlock
-	{
-		List<VirtualFile> myFiles = new ArrayList<>();
-	}
+public class Unity3dProjectChangeListener implements Disposable {
+    public static class DataBlock {
+        List<VirtualFile> myFiles = new ArrayList<>();
+    }
 
-	private final Project myProject;
-	private VirtualFilePointer myAssetsDirPointer;
+    private final Project myProject;
+    private VirtualFilePointer myAssetsDirPointer;
 
-	@Nonnull
-	private Future<?> myUpdateCheckTask = CompletableFuture.completedFuture(null);
+    @Nonnull
+    private Future<?> myUpdateCheckTask = CompletableFuture.completedFuture(null);
 
-	private volatile DataBlock myDataBlock = new DataBlock();
+    private volatile DataBlock myDataBlock = new DataBlock();
 
-	private final AtomicBoolean myActive = new AtomicBoolean();
+    private final AtomicBoolean myActive = new AtomicBoolean();
 
-	private final Set<FileType> mySourceFileTypes = new HashSet<>();
+    private final Set<FileType> mySourceFileTypes = new HashSet<>();
 
-	@Inject
-	public Unity3dProjectChangeListener(@Nonnull Project project, @Nonnull StartupManager startupManager, @Nonnull VirtualFileManager virtualFileManager)
-	{
-		myProject = project;
+    @Inject
+    public Unity3dProjectChangeListener(@Nonnull Project project, @Nonnull VirtualFileManager virtualFileManager) {
+        myProject = project;
 
-		if(project.isDefault())
-		{
-			return;
-		}
+        if (project.isDefault()) {
+            return;
+        }
 
-		for(Unity3dProjectSourceFileTypeFactory factory : Unity3dProjectSourceFileTypeFactory.EP_NAME.getExtensionList())
-		{
-			factory.registerFileTypes(mySourceFileTypes::add);
-		}
+        for (Unity3dProjectSourceFileTypeFactory factory : Unity3dProjectSourceFileTypeFactory.EP_NAME.getExtensionList()) {
+            factory.registerFileTypes(mySourceFileTypes::add);
+        }
 
-		myProject.getMessageBus().connect().subscribe(ModuleRootListener.class, new ModuleRootListener()
-		{
-			@Override
-			@RequiredReadAction
-			public void rootsChanged(ModuleRootEvent event)
-			{
-				checkAndRunIfNeed(Application.get().getLastUIAccess());
-			}
-		});
+        myProject.getMessageBus().connect().subscribe(ModuleRootListener.class, new ModuleRootListener() {
+            @Override
+            @RequiredReadAction
+            public void rootsChanged(ModuleRootEvent event) {
+                checkAndRunIfNeed();
+            }
+        });
 
-		myAssetsDirPointer = VirtualFilePointerManager.getInstance().create(StandardFileSystems.FILE_PROTOCOL_PREFIX + myProject.getPresentableUrl() + "/" + Unity3dProjectImporter
-				.ASSETS_DIRECTORY, this, null);
+        myAssetsDirPointer = VirtualFilePointerManager.getInstance().create(StandardFileSystems.FILE_PROTOCOL_PREFIX + myProject.getPresentableUrl() + "/" + Unity3dProjectImporter
+            .ASSETS_DIRECTORY, this, null);
 
-		virtualFileManager.addAsyncFileListener(new AsyncFileListener()
-		{
-			@Nullable
-			@Override
-			public ChangeApplier prepareChange(@Nonnull List<? extends VFileEvent> list)
-			{
-				List<VFileEvent> addOrMove = new ArrayList<>();
-				for(VFileEvent vFileEvent : list)
-				{
-					if(vFileEvent instanceof VFileCreateEvent || vFileEvent instanceof VFileMoveEvent)
-					{
-						addOrMove.add(vFileEvent);
-					}
-				}
-				if(addOrMove.isEmpty())
-				{
-					return null;
-				}
-				return new ChangeApplier()
-				{
-					@Override
-					public void afterVfsChange()
-					{
-						for(VFileEvent vFileEvent : addOrMove)
-						{
-							VirtualFile file = vFileEvent.getFile();
+        virtualFileManager.addAsyncFileListener(new AsyncFileListener() {
+            @Nullable
+            @Override
+            public ChangeApplier prepareChange(@Nonnull List<? extends VFileEvent> list) {
+                List<VFileEvent> addOrMove = new ArrayList<>();
+                for (VFileEvent vFileEvent : list) {
+                    if (vFileEvent instanceof VFileCreateEvent || vFileEvent instanceof VFileMoveEvent) {
+                        addOrMove.add(vFileEvent);
+                    }
+                }
+                if (addOrMove.isEmpty()) {
+                    return null;
+                }
+                return new ChangeApplier() {
+                    @Override
+                    public void afterVfsChange() {
+                        for (VFileEvent vFileEvent : addOrMove) {
+                            VirtualFile file = vFileEvent.getFile();
 
-							if(file != null)
-							{
-								handleChange(file);
-							}
-						}
-					}
-				};
-			}
-		}, this);
-		startupManager.registerPostStartupActivity(this::checkAndRunIfNeed);
-	}
+                            if (file != null) {
+                                handleChange(file);
+                            }
+                        }
+                    }
+                };
+            }
+        }, this);
+    }
 
-	@RequiredReadAction
-	private void checkAndRunIfNeed(UIAccess uiAccess)
-	{
-		myUpdateCheckTask.cancel(false);
-		myUpdateCheckTask = CompletableFuture.completedFuture(null);
+    public void checkAndRunIfNeed() {
+        myUpdateCheckTask.cancel(false);
+        myUpdateCheckTask = CompletableFuture.completedFuture(null);
 
-		myActive.set(Unity3dModuleExtensionUtil.getRootModuleExtension(myProject) != null);
+        myActive.set(ReadAction.compute(() -> Unity3dModuleExtensionUtil.getRootModuleExtension(myProject)) != null);
 
-		if(!myActive.get())
-		{
-			return;
-		}
+        if (!myActive.get()) {
+            return;
+        }
 
-		myUpdateCheckTask = AppExecutorUtil.getAppScheduledExecutorService().scheduleWithFixedDelay((Runnable) this::checkNotification, 10, 10, TimeUnit.SECONDS);
-	}
+        myUpdateCheckTask = AppExecutorUtil.getAppScheduledExecutorService().scheduleWithFixedDelay((Runnable) this::checkNotification, 10, 10, TimeUnit.SECONDS);
+    }
 
-	private void checkNotification()
-	{
-		if(DumbService.isDumb(myProject))
-		{
-			return;
-		}
+    private void checkNotification() {
+        if (DumbService.isDumb(myProject)) {
+            return;
+        }
 
-		DataBlock dataBlock = myDataBlock;
+        DataBlock dataBlock = myDataBlock;
 
-		myDataBlock = new DataBlock();
+        myDataBlock = new DataBlock();
 
-		if(dataBlock.myFiles.isEmpty())
-		{
-			return;
-		}
+        if (dataBlock.myFiles.isEmpty()) {
+            return;
+        }
 
-		UIUtil.invokeLaterIfNeeded(() ->
-		{
-			boolean needNotification = false;
+        UIUtil.invokeLaterIfNeeded(() ->
+        {
+            boolean needNotification = false;
 
-			for(VirtualFile file : dataBlock.myFiles)
-			{
-				Module module = ModuleUtilCore.findModuleForFile(file, myProject);
-				if(module == null)
-				{
-					continue;
-				}
+            for (VirtualFile file : dataBlock.myFiles) {
+                Module module = ModuleUtilCore.findModuleForFile(file, myProject);
+                if (module == null) {
+                    continue;
+                }
 
-				if(module == Unity3dModuleExtensionUtil.getRootModule(myProject))
-				{
-					needNotification = true;
-					break;
-				}
-			}
+                if (module == Unity3dModuleExtensionUtil.getRootModule(myProject)) {
+                    needNotification = true;
+                    break;
+                }
+            }
 
-			if(needNotification)
-			{
-				Notification notification = new Notification(UnityNotificationGroup.INSTANCE, Application.get().getName().get(), "Unity project structure changed", NotificationType.INFORMATION);
-				notification.addAction(new NotificationAction("Rebuild project")
-				{
-					@RequiredUIAccess
-					@Override
-					public void actionPerformed(@Nonnull AnActionEvent anActionEvent, @Nonnull Notification notification)
-					{
-						final Unity3dRootModuleExtension rootModuleExtension = Unity3dModuleExtensionUtil.getRootModuleExtension(myProject);
-						if(rootModuleExtension == null)
-						{
-							return;
-						}
+            if (needNotification) {
+                Notification notification = new Notification(UnityNotificationGroup.INSTANCE, Application.get().getName().get(), "Unity project structure changed", NotificationType.INFORMATION);
+                notification.addAction(new NotificationAction("Rebuild project") {
+                    @RequiredUIAccess
+                    @Override
+                    public void actionPerformed(@Nonnull AnActionEvent anActionEvent, @Nonnull Notification notification) {
+                        final Unity3dRootModuleExtension rootModuleExtension = Unity3dModuleExtensionUtil.getRootModuleExtension(myProject);
+                        if (rootModuleExtension == null) {
+                            return;
+                        }
 
-						Unity3dProjectImporter.syncProjectStep(myProject, rootModuleExtension.getSdk(), null, true);
-					}
-				});
-				notification.notify(myProject);
-			}
-		});
-	}
+                        Unity3dProjectImporter.syncProjectStep(myProject, rootModuleExtension.getSdk(), null, true);
+                    }
+                });
+                notification.notify(myProject);
+            }
+        });
+    }
 
-	private void handleChange(@Nonnull VirtualFile virtualFile)
-	{
-		if(!myActive.get())
-		{
-			return;
-		}
+    private void handleChange(@Nonnull VirtualFile virtualFile) {
+        if (!myActive.get()) {
+            return;
+        }
 
-		if(!mySourceFileTypes.contains(virtualFile.getFileType()))
-		{
-			return;
-		}
+        if (!mySourceFileTypes.contains(virtualFile.getFileType())) {
+            return;
+        }
 
-		String url = virtualFile.getUrl();
-		if(url.startsWith(myAssetsDirPointer.getUrl()))
-		{
-			myDataBlock.myFiles.add(virtualFile);
-		}
-	}
+        String url = virtualFile.getUrl();
+        if (url.startsWith(myAssetsDirPointer.getUrl())) {
+            myDataBlock.myFiles.add(virtualFile);
+        }
+    }
 
-	@Override
-	public void dispose()
-	{
-		myUpdateCheckTask.cancel(false);
-	}
+    @Override
+    public void dispose() {
+        myUpdateCheckTask.cancel(false);
+    }
 }
