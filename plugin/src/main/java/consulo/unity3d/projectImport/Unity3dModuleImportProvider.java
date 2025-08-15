bd290e603fc0e5b8536c2029e07eab0ad5f8c9f3
Valery Semenchuk <vistall.valeriy@gmail.com>
2025-08-15 19:08:23 +0400

    migrate to localize value

--- plugin/src/main/java/consulo/unity3d/projectImport/Unity3dModuleImportProvider.java
+++ plugin/src/main/java/consulo/unity3d/projectImport/Unity3dModuleImportProvider.java
@@ -20,19 +20,21 @@ import consulo.annotation.access.RequiredReadAction;
 import consulo.annotation.component.ExtensionImpl;
 import consulo.content.bundle.Sdk;
 import consulo.ide.moduleImport.ModuleImportProvider;
+import consulo.localize.LocalizeValue;
 import consulo.module.ModifiableModuleModel;
 import consulo.module.Module;
 import consulo.project.Project;
 import consulo.project.startup.StartupManager;
 import consulo.ui.ex.wizard.WizardStep;
 import consulo.ui.image.Image;
-import consulo.unity3d.Unity3dIcons;
+import consulo.unity3d.icon.Unity3dIconGroup;
 import consulo.unity3d.jsonApi.UnityOpenFilePostHandlerRequest;
+import consulo.unity3d.localize.Unity3dLocalize;
 import consulo.unity3d.projectImport.ui.Unity3dWizardStep;
-import jakarta.inject.Inject;
-
 import jakarta.annotation.Nonnull;
 import jakarta.annotation.Nullable;
+import jakarta.inject.Inject;
+
 import java.io.File;
 import java.util.function.Consumer;
 
@@ -41,79 +43,63 @@ import java.util.function.Consumer;
  * @since 29.12.14
  */
 @ExtensionImpl
-public class Unity3dModuleImportProvider implements ModuleImportProvider<UnityModuleImportContext>
-{
-	@Nullable
-	private Sdk myPredefinedSdk;
-	@Nullable
-	private UnityOpenFilePostHandlerRequest myRequest;
+public class Unity3dModuleImportProvider implements ModuleImportProvider<UnityModuleImportContext> {
+    @Nullable
+    private Sdk myPredefinedSdk;
+    @Nullable
+    private UnityOpenFilePostHandlerRequest myRequest;
 
-	@Inject
-	public Unity3dModuleImportProvider()
-	{
-	}
+    @Inject
+    public Unity3dModuleImportProvider() {
+    }
 
-	public Unity3dModuleImportProvider(@Nonnull Sdk predefinedSdk, @Nonnull UnityOpenFilePostHandlerRequest request)
-	{
-		myPredefinedSdk = predefinedSdk;
-		myRequest = request;
-	}
+    public Unity3dModuleImportProvider(@Nonnull Sdk predefinedSdk, @Nonnull UnityOpenFilePostHandlerRequest request) {
+        myPredefinedSdk = predefinedSdk;
+        myRequest = request;
+    }
 
-	@Nonnull
-	@Override
-	public UnityModuleImportContext createContext(@Nullable Project project)
-	{
-		UnityModuleImportContext context = new UnityModuleImportContext(project);
-		context.setSdk(myPredefinedSdk);
-		context.setRequestor(myRequest);
-		return context;
-	}
+    @Nonnull
+    @Override
+    public UnityModuleImportContext createContext(@Nullable Project project) {
+        UnityModuleImportContext context = new UnityModuleImportContext(project);
+        context.setSdk(myPredefinedSdk);
+        context.setRequestor(myRequest);
+        return context;
+    }
 
-	@Nonnull
-	@Override
-	public String getName()
-	{
-		return "Unity3D";
-	}
+    @Nonnull
+    @Override
+    public LocalizeValue getName() {
+        return Unity3dLocalize.unityName();
+    }
 
-	@Nonnull
-	@Override
-	public Image getIcon()
-	{
-		return Unity3dIcons.Unity3d;
-	}
+    @Nonnull
+    @Override
+    public Image getIcon() {
+        return Unity3dIconGroup.unity3d();
+    }
 
-	@Override
-	public boolean canImport(@Nonnull File fileOrDirectory)
-	{
-		return fileOrDirectory.isDirectory() && new File(fileOrDirectory, "ProjectSettings/ProjectSettings.asset").exists();
-	}
+    @Override
+    public boolean canImport(@Nonnull File fileOrDirectory) {
+        return fileOrDirectory.isDirectory() && new File(fileOrDirectory, "ProjectSettings/ProjectSettings.asset").exists();
+    }
 
-	@RequiredReadAction
-	@Override
-	public void process(@Nonnull UnityModuleImportContext context, @Nonnull Project project, @Nonnull ModifiableModuleModel model, @Nonnull Consumer<Module> newModuleConsumer)
-	{
-		Sdk unitySdk = context.getSdk();
-		UnityOpenFilePostHandlerRequest requestor = context.getRequestor();
+    @RequiredReadAction
+    @Override
+    public void process(@Nonnull UnityModuleImportContext context, @Nonnull Project project, @Nonnull ModifiableModuleModel model, @Nonnull Consumer<Module> newModuleConsumer) {
+        Sdk unitySdk = context.getSdk();
+        UnityOpenFilePostHandlerRequest requestor = context.getRequestor();
 
-		Module rootModule = model.newModule(project.getName(), project.getBasePath());
+        Module rootModule = model.newModule(project.getName(), project.getBasePath());
 
-		newModuleConsumer.accept(rootModule);
+        newModuleConsumer.accept(rootModule);
 
-		StartupManager.getInstance(project).registerPostStartupActivity(() -> Unity3dProjectImporter.syncProjectStep(project, unitySdk, requestor, true));
+        StartupManager.getInstance(project).registerPostStartupActivity(() -> Unity3dProjectImporter.syncProjectStep(project, unitySdk, requestor, true));
 
-	}
+    }
 
-	@Override
-	public void buildSteps(@Nonnull Consumer<WizardStep<UnityModuleImportContext>> consumer, @Nonnull UnityModuleImportContext context)
-	{
-		consumer.accept(new Unity3dWizardStep(context));
-	}
-
-	@Nonnull
-	@Override
-	public String getFileSample()
-	{
-		return "<b>Unity3D</b> project";
-	}
+    @Override
+    public void buildSteps(@Nonnull Consumer<WizardStep<UnityModuleImportContext>> consumer, @Nonnull UnityModuleImportContext context) {
+        consumer.accept(new Unity3dWizardStep(context));
+    }
 }
