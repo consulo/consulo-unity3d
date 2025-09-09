6ccc743d3877a42f499138e6f4e7787cc6307b4d
Valery Semenchuk <vistall.valeriy@gmail.com>
2025-09-09 19:04:59 +0400

    fix compile after migration to new api

--- plugin/src/main/java/consulo/unity3d/run/before/UnityRefreshBeforeRunTaskProvider.java
+++ plugin/src/main/java/consulo/unity3d/run/before/UnityRefreshBeforeRunTaskProvider.java
@@ -24,21 +24,21 @@ import consulo.document.FileDocumentManager;
 import consulo.execution.BeforeRunTaskProvider;
 import consulo.execution.configuration.RunConfiguration;
 import consulo.execution.runner.ExecutionEnvironment;
+import consulo.localize.LocalizeValue;
 import consulo.project.ui.notification.Notification;
 import consulo.project.ui.notification.NotificationType;
 import consulo.ui.UIAccess;
 import consulo.ui.annotation.RequiredUIAccess;
 import consulo.ui.image.Image;
-import consulo.unity3d.Unity3dIcons;
 import consulo.unity3d.UnityNotificationGroup;
 import consulo.unity3d.editor.UnityEditorCommunication;
 import consulo.unity3d.editor.UnityRefresh;
+import consulo.unity3d.icon.Unity3dIconGroup;
 import consulo.unity3d.jsonApi.UnityPingPong;
 import consulo.unity3d.run.test.Unity3dTestConfiguration;
 import consulo.util.concurrent.AsyncResult;
 import consulo.util.dataholder.Key;
 import consulo.util.lang.TimeoutUtil;
-
 import jakarta.annotation.Nonnull;
 import jakarta.annotation.Nullable;
 
@@ -47,101 +47,88 @@ import jakarta.annotation.Nullable;
  * @since 21.01.2016
  */
 @ExtensionImpl
-public class UnityRefreshBeforeRunTaskProvider extends BeforeRunTaskProvider<UnityRefreshBeforeRunTask>
-{
-	private static final Key<UnityRefreshBeforeRunTask> ourKey = Key.create("unity.refresh.task");
+public class UnityRefreshBeforeRunTaskProvider extends BeforeRunTaskProvider<UnityRefreshBeforeRunTask> {
+    private static final Key<UnityRefreshBeforeRunTask> ourKey = Key.create("unity.refresh.task");
 
-	@Nonnull
-	@Override
-	public Key<UnityRefreshBeforeRunTask> getId()
-	{
-		return ourKey;
-	}
+    @Nonnull
+    @Override
+    public Key<UnityRefreshBeforeRunTask> getId() {
+        return ourKey;
+    }
 
-	@Nullable
-	@Override
-	public Image getIcon()
-	{
-		return Unity3dIcons.Unity3d;
-	}
+    @Nullable
+    @Override
+    public Image getIcon() {
+        return Unity3dIconGroup.unity3d();
+    }
 
-	@Nonnull
-	@Override
-	public String getName()
-	{
-		return "UnityEditor refresh";
-	}
+    @Nonnull
+    @Override
+    public LocalizeValue getName() {
+        return LocalizeValue.localizeTODO("UnityEditor refresh");
+    }
 
-	@Override
-	public boolean isConfigurable()
-	{
-		return false;
-	}
+    @Override
+    public boolean isConfigurable() {
+        return false;
+    }
 
-	@Nullable
-	@Override
-	public UnityRefreshBeforeRunTask createTask(RunConfiguration runConfiguration)
-	{
-		return runConfiguration instanceof Unity3dTestConfiguration ? new UnityRefreshBeforeRunTask(ourKey) : null;
-	}
+    @Nullable
+    @Override
+    public UnityRefreshBeforeRunTask createTask(RunConfiguration runConfiguration) {
+        return runConfiguration instanceof Unity3dTestConfiguration ? new UnityRefreshBeforeRunTask(ourKey) : null;
+    }
 
-	@RequiredUIAccess
-	@Nonnull
-	@Override
-	public AsyncResult<Void> configureTask(RunConfiguration runConfiguration, UnityRefreshBeforeRunTask task)
-	{
-		return AsyncResult.rejected();
-	}
+    @RequiredUIAccess
+    @Nonnull
+    @Override
+    public AsyncResult<Void> configureTask(RunConfiguration runConfiguration, UnityRefreshBeforeRunTask task) {
+        return AsyncResult.rejected();
+    }
 
-	@Nonnull
-	@Override
-	public AsyncResult<Void> executeTaskAsync(UIAccess uiAccess, DataContext context, RunConfiguration configuration, ExecutionEnvironment env, UnityRefreshBeforeRunTask task)
-	{
-		AsyncResult<Void> result = AsyncResult.undefined();
+    @Nonnull
+    @Override
+    public AsyncResult<Void> executeTaskAsync(UIAccess uiAccess, DataContext context, RunConfiguration configuration, ExecutionEnvironment env, UnityRefreshBeforeRunTask task) {
+        AsyncResult<Void> result = AsyncResult.undefined();
 
-		uiAccess.give(() ->
-		{
-			FileDocumentManager.getInstance().saveAllDocuments();
+        uiAccess.give(() ->
+        {
+            FileDocumentManager.getInstance().saveAllDocuments();
 
-			Task.Backgroundable.queue(env.getProject(), "Queue UnityEditor refresh", true, indicator -> {
-				boolean[] receiveData = new boolean[1];
+            Task.Backgroundable.queue(env.getProject(), "Queue UnityEditor refresh", true, indicator -> {
+                boolean[] receiveData = new boolean[1];
 
-				UnityRefresh postObject = new UnityRefresh();
+                UnityRefresh postObject = new UnityRefresh();
 
-				UnityPingPong.Token<Boolean> accessToken = UnityPingPong.wantReply(postObject.uuid, o -> {
-					if(o)
-					{
-						result.setDone();
-					}
-					else
-					{
-						result.setRejected();
-					}
-					receiveData[0] = o;
-				});
+                UnityPingPong.Token<Boolean> accessToken = UnityPingPong.wantReply(postObject.uuid, o -> {
+                    if (o) {
+                        result.setDone();
+                    }
+                    else {
+                        result.setRejected();
+                    }
+                    receiveData[0] = o;
+                });
 
-				boolean request = UnityEditorCommunication.request(env.getProject(), postObject, true);
-				if(!request)
-				{
-					new Notification(UnityNotificationGroup.INSTANCE, Application.get().getName().get(), "UnityEditor is not responding", NotificationType.INFORMATION).notify(env.getProject());
+                boolean request = UnityEditorCommunication.request(env.getProject(), postObject, true);
+                if (!request) {
+                    new Notification(UnityNotificationGroup.INSTANCE, Application.get().getName().get(), "UnityEditor is not responding", NotificationType.INFORMATION).notify(env.getProject());
 
-					accessToken.finish(Boolean.FALSE);
-					return;
-				}
+                    accessToken.finish(Boolean.FALSE);
+                    return;
+                }
 
-				while(!receiveData[0])
-				{
-					if(indicator.isCanceled())
-					{
-						accessToken.finish(Boolean.FALSE);
-						break;
-					}
+                while (!receiveData[0]) {
+                    if (indicator.isCanceled()) {
+                        accessToken.finish(Boolean.FALSE);
+                        break;
+                    }
 
-					TimeoutUtil.sleep(500L);
-				}
-			});
-		}).doWhenRejectedWithThrowable(result::rejectWithThrowable);
+                    TimeoutUtil.sleep(500L);
+                }
+            });
+        }).doWhenRejectedWithThrowable(result::rejectWithThrowable);
 
-		return result;
-	}
+        return result;
+    }
 }
