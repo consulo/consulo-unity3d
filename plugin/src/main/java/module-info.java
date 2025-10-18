8c5477819da2a57dabcbdf1c91c376c4eca0ef8a
Valery Semenchuk <vistall.valeriy@gmail.com>
2025-10-18 21:46:44 +0400

    add missed opens

--- plugin/src/main/java/module-info.java
+++ plugin/src/main/java/module-info.java
@@ -4,46 +4,46 @@
  */
 module consulo.unity3d
 {
-	// TODO remove in future
-	requires java.desktop;
+    // TODO remove in future
+    requires java.desktop;
 
-	requires consulo.game.framework.api;
+    requires consulo.game.framework.api;
 
-	requires consulo.ide.api;
+    requires consulo.ide.api;
 
-	requires consulo.language.impl;
+    requires consulo.language.impl;
 
-	requires consulo.unity3d.shaderlab;
+    requires consulo.unity3d.shaderlab;
 
-	requires consulo.unity3d.cg.shader;
+    requires consulo.unity3d.cg.shader;
 
-	requires consulo.csharp.psi.impl;
-	requires consulo.csharp;
+    requires consulo.csharp.psi.impl;
+    requires consulo.csharp;
 
-	requires consulo.nunit.api;
+    requires consulo.nunit.api;
 
-	requires consulo.javascript.base.api;
-	requires consulo.javascript.json.javascript.impl;
+    requires consulo.javascript.base.api;
+    requires consulo.javascript.json.javascript.impl;
 
-	requires org.jetbrains.plugins.yaml;
+    requires org.jetbrains.plugins.yaml;
 
-	requires consulo.dotnet.mono.debugger.impl;
-	requires mono.soft.debugging;
+    requires consulo.dotnet.mono.debugger.impl;
+    requires mono.soft.debugging;
 
-	requires com.sun.jna;
-	requires com.sun.jna.platform;
+    requires com.sun.jna;
+    requires com.sun.jna.platform;
 
-	requires com.google.gson;
+    requires com.google.gson;
 
-	requires org.apache.httpcomponents.httpcore;
-	requires org.apache.httpcomponents.httpmime;
+    requires org.apache.httpcomponents.httpcore;
+    requires org.apache.httpcomponents.httpmime;
 
-	requires dd.plist;
+    requires dd.plist;
 
-	opens consulo.unity3d.usages to consulo.util.xml.serializer;
-	opens consulo.unity3d.run to consulo.util.xml.serializer;
-	opens consulo.unity3d.editor to com.google.gson;
-	opens consulo.unity3d.packages to com.google.gson, consulo.application.impl;
-	opens consulo.unity3d.jsonApi to com.google.gson;
-	
+    opens consulo.unity3d.usages to consulo.util.xml.serializer;
+    opens consulo.unity3d.run to consulo.util.xml.serializer;
+    opens consulo.unity3d.editor to com.google.gson;
+    opens consulo.unity3d.packages to com.google.gson, consulo.application.impl;
+    opens consulo.unity3d.jsonApi to com.google.gson;
+    opens consulo.unity3d.projectImport.newImport to com.google.gson;
 }
